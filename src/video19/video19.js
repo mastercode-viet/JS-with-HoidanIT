5a167bc370ec7130109e3fac7c5a3b7e6133e2ca
mastercode-viet <quachviet010@gmail.com>
2025-09-14 17:14:45 +0700

    tham chieu trong js

---
+++ src/video19/video19.js
@@ -0,0 +1,14 @@
+console.log("video 19");
+
+// kiểu dữ liệu tham chiếu
+
+// object
+const person = {
+  name: "Eric",
+  age: 25,
+};
+console.log(person);
+
+// array
+const diem = [1, 4, 19];
+console.log(diem);

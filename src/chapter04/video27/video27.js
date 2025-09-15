59be48804e8596f13362dbe483e4309a1f7a6187
mastercode-viet <quachviet010@gmail.com>
2025-09-15 17:40:22 +0700

    switch...case

---
+++ src/chapter04/video27/video27.js
@@ -0,0 +1,21 @@
+let grade = "B";
+
+switch (grade) {
+  case "A":
+    console.log("Xuất sắc!");
+    break;
+  case "B":
+    console.log("Giỏi!");
+    break;
+  case "C":
+    console.log("Khá!");
+    break;
+  case "D":
+    console.log("Trung bình!");
+    break;
+  case "F":
+    console.log("Rớt môn!");
+    break;
+  default:
+    console.log("Điểm không hợp lệ!");
+}

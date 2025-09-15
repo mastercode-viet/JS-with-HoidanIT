4916026e05d99a58c058c7f0ce2f4ed6ce44d894
mastercode-viet <quachviet010@gmail.com>
2025-09-15 23:23:41 +0700

    for, while, do-while

---
+++ src/chapter04/video28/video28.js
@@ -0,0 +1,15 @@
+console.log("video 28");
+
+// for
+for (let i = 0; i <= 5; i++) {
+  console.log("i = ", i);
+}
+
+// while
+let diem = 9;
+while (diem >= 5) {
+  console.log(">>>check diem ", diem);
+  diem--;
+}
+
+// do while : dù điều kiện đúng hay sai nó vẫn thực hiện 1 lần
